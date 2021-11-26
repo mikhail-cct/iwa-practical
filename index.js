af25e47431b6d17eaef2c6aa28db061f7fb7f431
mikhail-cct <tayemnyj+cct@gmail.com>
2021-11-26 11:11:40 +0000

    finished index.js

--- index.js
+++ index.js
@@ -11,3 +11,28 @@ const   router = express(),
 
 router.use(express.static(path.resolve(__dirname,'views'))); //Serving static content from "views" folder
 
+router.get('/', function(req, res) {
+
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('PaddysCafe.xml', 'utf8'),
+        xsl = fs.readFileSync('PaddysCafe.xsl', 'utf8');
+
+    console.log(xml);
+    console.log(xsl);
+
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+
+    let result = xsltProcess(doc, stylesheet);
+
+    console.log(result);
+
+    res.end(result.toString());
+
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+    const addr = server.address();
+    console.log('Server listening at', addr.address + ':' + addr.port)
+});
