9ae0692bc665342794797b1bb1c8d8b7144a51b2
mikhail-cct <tayemnyj+cct@gmail.com>
2021-11-26 11:15:25 +0000

    comments in index.js

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ const   express = require('express'), //Allows to respond to HTTP requests, defi
         xmlParse = require('xslt-processor').xmlParse, //Parsing XML
         xsltProcess = require('xslt-processor').xsltProcess; //Processing XSLT
 
-const   router = express(),
-        server = http.createServer(router);
+const   router = express(), //Instantiating Express
+        server = http.createServer(router); //Instantiating the server
 
 router.use(express.static(path.resolve(__dirname,'views'))); //Serving static content from "views" folder
 
 router.get('/', function(req, res) {
 
-    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.writeHead(200, {'Content-Type' : 'text/html'}); //Tell the user that the resource exists and which type that is
 
-    let xml = fs.readFileSync('PaddysCafe.xml', 'utf8'),
-        xsl = fs.readFileSync('PaddysCafe.xsl', 'utf8');
+    let xml = fs.readFileSync('PaddysCafe.xml', 'utf8'), //read in the XML file
+        xsl = fs.readFileSync('PaddysCafe.xsl', 'utf8'); //read in the XSL file
 
     console.log(xml);
     console.log(xsl);
 
-    let doc = xmlParse(xml),
-        stylesheet = xmlParse(xsl);
+    let doc = xmlParse(xml), //Parse the XML file
+        stylesheet = xmlParse(xsl); //Parse the XSL file
 
-    let result = xsltProcess(doc, stylesheet);
+    let result = xsltProcess(doc, stylesheet); //Performing XSLT
 
     console.log(result);
 
-    res.end(result.toString());
+    res.end(result.toString()); //Serve back the user
 
 });
 
