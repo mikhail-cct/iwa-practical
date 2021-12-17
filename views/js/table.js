0b4d6ae52f80c26ee53360347b4b034572f8088f
mikhail-cct <tayemnyj+cct@gmail.com>
2021-12-17 09:52:25 +0000

    wk13 select_row() & xsl update

--- views/js/table.js
+++ views/js/table.js
@@ -7,12 +7,13 @@ function draw_table(){
             cache: false,
             success: function(html) {
                 $("#results").append(html);
+                select_row();
             }
         });
 
     };
     $.getHTMLuncached("/get/html");
-}
+};
 
 
 function append(){
@@ -24,9 +25,22 @@ function append(){
         data: '{"sec_n": "' + $("#section").val() + '", "item":"' + $("#item").val() + '", "price":"' + $("#price").val() + '"}',
         async: false,
         success: setTimeout(draw_table, 1000)
-    })
+    });
 };
 
+function select_row()
+{
+    $("#menuTable tbody tr[id]").click(function ()
+    {
+        $(".selected").removeClass("selected");
+        $(this).addClass("selected");
+        var section = $(this).prevAll("tr").children("rd[colspan='3']").length - 1;
+        var entry = $(this).attr("id") - 1;
+        delete_row(section, entry);
+
+    })
+
+};
 
 $(document).ready(function(){
     draw_table();
